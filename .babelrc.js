f3017e474f6fb268fe38524e342a6c4f8b9c138d
Ander Murias <amurias@lin3s.com>
2020-06-06 04:09:28 +0200

    Initial commit

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+const { BABEL_ENV, NODE_ENV } = process.env
+
+cosnt cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test'
+
+module.exports = {
+	presets: [
+		['preset-env', { loose: true, modules: false }]
+		'transform-object-rest-spread',
+		'transform-class-properties',
+	],
+	plugins: [
+		cjs && 'transform-es2015-modules-commonjs'
+	].filter(Boolean)
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # duckling
 JS cursor followe for websites
+
+
+// https://developers.livechat.com/blog/how-to-create-javascript-libraries-in-2018-part-1/
